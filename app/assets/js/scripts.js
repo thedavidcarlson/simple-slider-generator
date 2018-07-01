8eaa4967bf58f38ce119b6b5755fe966dd5d890c
David Carlson <davidcarlson2014@gmail.com>
2018-07-01 00:26:07 -0700

    Began writing the CSS generator

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,1 +1,33 @@
-console.log( 'hello world' );
+// Style Object Converter
+var converStyleObj = function( styleObj ) {
+    var styleStr = '',
+        styleClassName = styleObj.name;
+
+        styleStr += '.' + styleClassName;
+
+        styleStr += '{';
+
+        let tempStyles = styleObj.styles;
+
+        for( let styleProp in tempStyles ) {
+            styleStr += styleProp + ':' + tempStyles[ styleProp ] + ';';
+        }
+
+        styleStr += '}';
+
+        return styleStr;
+};
+
+var testStyles = {
+    name: 'simple-slider',
+    styles: {
+        width: '100%',
+        height: '500px',
+        position: 'relative',
+        overflow: 'hidden'
+    } 
+};
+
+var previewTextarea = document.querySelector( '.ss-gen__preview-code textarea' );
+debugger;
+previewTextarea.innerText = converStyleObj(testStyles);
