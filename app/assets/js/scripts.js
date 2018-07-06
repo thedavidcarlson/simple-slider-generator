172498021f1f8da669e82e7654104d87c7ed23a4
unknown <davidcarlson2014@gmail.com>
2018-07-06 12:22:48 -0700

    Began making slide count dynamic

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,6 +1,27 @@
 // Global vars
-let previewTextarea = document.querySelector( '.ss-gen__preview-code textarea' ),
-    styleStr = '';
+// Dummy data representing the slides
+let slideObjs = [ 
+        {
+            'headerText': 'Some header text',
+            'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
+            'imgLink': 'https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_1280.jpg'
+        },
+        {
+            'headerText': 'Some header text',
+            'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
+            'imgLink': 'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'
+        },
+        {
+            'headerText': 'Some header text',
+            'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
+            'imgLink': 'https://cdn.pixabay.com/photo/2018/05/28/23/35/sunset-3437560_1280.jpg'
+        },
+        {
+            'headerText': 'Some header text',
+            'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
+            'imgLink': 'https://cdn.pixabay.com/photo/2018/05/13/21/55/water-3398111_1280.jpg'
+        }
+    ];
 
 /*
  * @function: Takes in an object that defines styles and converts it to CSS. Object can be standard CSS or a keyframe object.
@@ -98,45 +119,58 @@ let addControls = function( controlCount ) {
     document.querySelector( '.simple-slider__control' ).appendChild( controlWrapper );
 };
 
-// Generate the standard styles using an object that represents CSS with class names as properties defined before this file.
-styleStr += convertCssObj( defaultStyles );
+let generateSlider = function() {
+    let previewTextarea = document.querySelector( '.ss-gen__preview-code textarea' ),
+        styleStr = '';
 
-// Generate the animation styles using an object that represents CSS with animation names as properties defined before this file.
-styleStr += convertCssObj( animationStyles, true /*Keyframe Styles*/ );
+    // Generate the standard styles using an object that represents CSS with class names as properties defined before this file.
+    styleStr += convertCssObj( defaultStyles );
 
-// Set preview styles
-document.querySelector( '.ss-gen__preview-styles' ).innerHTML = styleStr;
+    // Generate the animation styles using an object that represents CSS with animation names as properties defined before this file.
+    styleStr += convertCssObj( animationStyles, true /*Keyframe Styles*/ );
 
-// Dummy data representing the slides
-let slideObjs = [ 
-    {
-        'headerText': 'Some header text',
-        'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
-        'imgLink': 'https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_1280.jpg'
-    },
-    {
-        'headerText': 'Some header text',
-        'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
-        'imgLink': 'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'
-    },
-    {
-        'headerText': 'Some header text',
-        'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
-        'imgLink': 'https://cdn.pixabay.com/photo/2018/05/28/23/35/sunset-3437560_1280.jpg'
-    },
-    {
-        'headerText': 'Some header text',
-        'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
-        'imgLink': 'https://cdn.pixabay.com/photo/2018/05/13/21/55/water-3398111_1280.jpg'
-    }
-];
+    // Set preview styles
+    document.querySelector( '.ss-gen__preview-styles' ).innerHTML = styleStr;
 
-// Generate and append the slides
-addSlides( slideObjs );
+    // Generate and append the slides
+    addSlides( slideObjs );
 
-// Add the styles and markup to the textarea
-previewTextarea.innerText = '<style type="text/css">' 
-                            + styleStr 
-                            + '</style>' 
-                            + document.querySelector( '.ss-gen__preview-slider' ).innerHTML.trim();
+    // Add the styles and markup to the textarea
+    previewTextarea.innerText = '<style type="text/css">' 
+                                + styleStr 
+                                + '</style>' 
+                                + document.querySelector( '.ss-gen__preview-slider' ).innerHTML.trim();
+};
 
+let createAnimationDefinition = function( numSlides, animationName, propertyName ) {
+    // let animationObj = {
+    //     '0%': {
+    //         'margin-left': '0'
+    //     }
+    // };
+
+    // for( let i = 0; i < numSlides; i++ ) {
+
+    // }
+};
+
+// Event Bindings
+// document.querySelector( '.ss-gen__add-slide-control' ).onClick = function( ev ) {
+//     if( slideObjs.length < 10 ) {
+//         slideObjs.push( {
+//             'headerText': 'Some header text',
+//             'bodyText': 'This is a short description of nothing. This text really has no purpose other than to occupy space. This space must be occupied to showcase the slider.',
+//             'imgLink': 'https://cdn.pixabay.com/photo/2018/05/13/21/55/water-3398111_1280.jpg'
+//         } );
+
+//         let numSlides = slideObjs.length;
+
+//         defaultStyles[ '~simple-slider__slides' ].width = ( numSlides * 100 ) + '%';
+//         defaultStyles[ '~simple-slider__slide' ].width = ( 100 / numSlides ) + '%';
+//         defaultStyles[ '~simple-slider__text' ].animation = 'text-animation ' + ( 30 / numSlides )  + 's infinite';
+//     } else {
+//         console.log( 'Can\'t add more than 10 slides' );
+//     }
+// };
+
+generateSlider();
--- app/assets/js/styleObjects.js
+++ app/assets/js/styleObjects.js
@@ -6,14 +6,16 @@ var defaultStyles = {
         overflow: 'hidden'
     },
     '~simple-slider__slides': {
+        /* 100 * n */
         width: '400%',
         height: '100%',
         position: 'relative',
-        '-webkit-animation': 'slide-animation 30s infinite',
-        '-moz-animation': 'slide-animation 30s infinite',
+        /*'-webkit-animation': 'slide-animation 30s infinite',
+        '-moz-animation': 'slide-animation 30s infinite',*/
         animation: 'slide-animation 30s infinite'
     },
     '~simple-slider__slide': {
+        /* 100 / n */
         width: '25%',
         height: '100%',
         float: 'left',
@@ -52,8 +54,9 @@ var defaultStyles = {
         'box-sizing': 'border-box',
         'font-family': 'Tahoma, Geneva, sans-serif',
         color: '#fff',
-        '-webkit-animation': 'text-animation 7.5s infinite',
-        '-moz-animation': 'text-animation 7.5s infinite',
+        /* animation length / n */
+        /*'-webkit-animation': 'text-animation 7.5s infinite',
+        '-moz-animation': 'text-animation 7.5s infinite',*/
         animation: 'text-animation 7.5s infinite'
     },
     '~simple-slider__text h1': {
@@ -96,8 +99,8 @@ var defaultStyles = {
         'border-radius': '50%',
         background: '#fff',
         position: 'relative',
-        '-webkit-animation': 'control-animation 30s infinite',
-        '-moz-animation': 'control-animation 30s infinite',
+        /*'-webkit-animation': 'control-animation 30s infinite',
+        '-moz-animation': 'control-animation 30s infinite',*/
         animation: 'control-animation 30s infinite'
     }
 };
@@ -111,64 +114,24 @@ var animationStyles = {
             left: '0'
         }
     },
-    '@-webkit-keyframes slide-animation': {
+    '@keyframes text-animation': {
         '0%': {
-            'margin-left': '0'
+            left: '-520px'
         },
-        '21%': {
-            'margin-left': '0'
+        '10%': {
+            left: '0'
         },
-        '25%': {
-            'margin-left': '-100%'
+        '70%': {
+            left: '0'
         },
-        '46%': {
-            'margin-left': '-100%'
-        },
-        '50%': {
-            'margin-left': '-200%'
-        },
-        '71%': {
-            'margin-left': '-200%'
-        },
-        '75%': {
-            'margin-left': '-300%'
-        },
-        '96%': {
-            'margin-left': '-300%'
+        '80%': {
+            left: '-520px'
         },
         '100%': {
-            'margin-left': '0'
+            left: '-520px'
         }
     },
-    '@-moz-keyframes slide-animation': {
-        '0%': {
-            'margin-left': '0'
-        },
-        '21%': {
-            'margin-left': '0'
-        },
-        '25%': {
-            'margin-left': '-100%'
-        },
-        '46%': {
-            'margin-left': '-100%'
-        },
-        '50%': {
-            'margin-left': '-200%'
-        },
-        '71%': {
-            'margin-left': '-200%'
-        },
-        '75%': {
-            'margin-left': '-300%'
-        },
-        '96%': {
-            'margin-left': '-300%'
-        },
-        '100%': {
-            'margin-left': '0'
-        }
-    },
+    /* 100/n% (plus one that is 4% less), margin-left 100 * (n - 1) */
     '@keyframes slide-animation': {
         '0%': {
             'margin-left': '0'
@@ -193,163 +156,6 @@ var animationStyles = {
         },
         '96%': {
             'margin-left': '-300%'
-        },
-        '100%': {
-            'margin-left': '0'
-        }
-    },
-    '@-webkit-keyframes text-animation': {
-        '0%': {
-            left: '-520px'
-        },
-        '10%': {
-            left: '0'
-        },
-        '30%': {
-            left: '0'
-        },
-        '40%': {
-            left: '0'
-        },
-        '50%': {
-            left: '0'
-        },
-        '60%': {
-            left: '0'
-        },
-        '70%': {
-            left: '0'
-        },
-        '80%': {
-            left: '-520px'
-        },
-        '90%': {
-            left: '-520px'
-        },
-        '100%': {
-            left: '-520px'
-        }
-    },
-    '@-moz-keyframes text-animation': {
-        '0%': {
-            left: '-520px'
-        },
-        '10%': {
-            left: '0'
-        },
-        '30%': {
-            left: '0'
-        },
-        '40%': {
-            left: '0'
-        },
-        '50%': {
-            left: '0'
-        },
-        '60%': {
-            left: '0'
-        },
-        '70%': {
-            left: '0'
-        },
-        '80%': {
-            left: '-520px'
-        },
-        '90%': {
-            left: '-520px'
-        },
-        '100%': {
-            left: '-520px'
-        }
-    },
-    '@keyframes text-animation': {
-        '0%': {
-            left: '-520px'
-        },
-        '10%': {
-            left: '0'
-        },
-        '15%': {
-            left: '0'
-        },
-        '30%': {
-            left: '0'
-        },
-        '40%': {
-            left: '0'
-        },
-        '50%': {
-            left: '0'
-        },
-        '60%': {
-            left: '0'
-        },
-        '70%': {
-            left: '0'
-        },
-        '80%': {
-            left: '-520px'
-        },
-        '90%': {
-            left: '-520px'
-        },
-        '100%': {
-            left: '-520px'
-        }
-    },
-    '@-webkit-keyframes control-animation': {
-       '0%': {
-            'margin-left': '0'
-        },
-        '21%': {
-            'margin-left': '0'
-        },
-        '25%': {
-            'margin-left': '15px'
-        },
-        '46%': {
-            'margin-left': '15px'
-        },
-        '50%': {
-            'margin-left': '30px'
-        },
-        '71%': {
-            'margin-left': '30px'
-        },
-        '75%': {
-            'margin-left': '45px'
-        },
-        '96%': {
-            'margin-left': '45px'
-        },
-        '100%': {
-            'margin-left': '0'
-        }
-    },
-    '@-moz-keyframes control-animation': {
-        '0%': {
-            'margin-left': '0'
-        },
-        '21%': {
-            'margin-left': '0'
-        },
-        '25%': {
-            'margin-left': '15px'
-        },
-        '46%': {
-            'margin-left': '15px'
-        },
-        '50%': {
-            'margin-left': '30px'
-        },
-        '71%': {
-            'margin-left': '30px'
-        },
-        '75%': {
-            'margin-left': '45px'
-        },
-        '96%': {
-            'margin-left': '45px'
         },
         '100%': {
             'margin-left': '0'
